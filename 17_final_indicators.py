c18137e6ddcb8316ae4edba59afc88c3e7848574
Carl Higgs <carl.higgs@rmit.edu.au>
2018-07-17 18:04:51 +1000

    made an ind_study_region_matrix.csv file cross listing indicators with the locales that they pertain to

--- 17_final_indicators.py
+++ 17_final_indicators.py
@@ -22,17 +22,18 @@ task = 'create destination indicator tables'
 # for web --- schema domain_idx_state_city_year
 ## Note ambiguity b/w parcels and dwellings --- I will aim to produce two outputs (maybe three) for each ind: residential parcels (this may be our approximation to cadastre?); residential dwellings (based on Meshblock dwelling counts; Maybe in addition, residents (based on MB persons)).
 
+
 # KNIME 
   # walk_1_vic_melb_2016
   ## % of street blocks with a perimeter of < 720 m (i.e. between 120 m and 240 m long and 60 m and 120 m wide)
 
 # walk_2_vic_melb_2016
 ## < / >= 80% dwellings < 1 km of an activity centre (with a supermarket?? written, but a necessary condition of activity centre, i think?)
-(CASE WHEN ind_activity.distance <  1000 THEN 1
+walk_2_vic_melb_2016 = '''(CASE WHEN ind_activity.distance <  1000 THEN 1
       WHEN ind_activity.distance >= 1000 THEN 0
-      ELSE NULL END) as walk_12_state_locale_2016_hard,
-1-1/(1+exp(-5*(ind_activity.distance-1000)/1000::double precision)) AS walk_12_state_locale_2016_soft,
-
+      ELSE NULL END) as walk_12_{region}_{locale}_2016_hard,
+1-1/(1+exp(-5*(ind_activity.distance-1000)/1000::double precision)) AS walk_12_{region}_{locale}_2016_soft,
+'''
 
 # KNIME 
   # walk_3_vic_melb_2016
@@ -67,7 +68,8 @@ task = 'create destination indicator tables'
 
 # walk_12_state_locale_2016
 ## Average distance to closest activity centre, where activity centre is defined as a supermarket < a commercial zoned Mesh Block
-ind_activity.distance AS walk_12_state_locale_2016,
+walk_12_state_locale_2016 = '''ind_activity.distance AS walk_12_{region}_{locale}_2016,
+'''
 
 ## Bec workflow
   # walk_13_state_locale_2016
@@ -75,30 +77,39 @@ ind_activity.distance AS walk_12_state_locale_2016,
   
 # walk_14_state_locale_2016
 ## average number of daily living types present, measured as a score of 0-3, with 1 point for each category of (convenience store/petrol station/newsagent, PT stop, supermarket) < 1600m network distance
-ind_daily_living.dl_hard AS walk_14_state_locale_2016_hard,
-ind_daily_living.dl_soft AS walk_14_state_locale_2016_soft,
+walk_14_state_locale_2016 = '''ind_daily_living.dl_hard AS walk_14_{region}_{locale}_2016_hard,
+ind_daily_living.dl_soft AS walk_14_{region}_{locale}_2016_soft,
+'''
 
 # walk_15_state_locale_2016
 ## street connectivity, measured as 3+ leg intersection count/area in km2, where the area is computed from the 1600m pedestrian-accessible street network buffered by 50m
-sc_nh1600m.sc_nh1600m AS walk_15_state_locale_2016,
+walk_15_state_locale_2016 = '''sc_nh1600m.sc_nh1600m AS walk_15_{region}_{locale}_2016,
+'''
 
 # walk_16_state_locale_2016
 ## dwelling density, calculated as the aggregate sum of dwellings recorded in the meshblock polygons intersecting the 1600m pedestrian-accessible street network once buffered by 50m ("neighbourhood") and divided by the "neighbourhood" area in hectares.
-dd_nh1600m.dd_nh1600m AS walk_16_state_locale_2016,
+walk_16_state_locale_2016 = '''dd_nh1600m.dd_nh1600m AS walk_16_state_locale_2016,
+'''
 
 # walk_17_state_locale_2016
 ## composite walkability index, combining street connectivity, dwelling density and daily living destinations
-ind_walkability.wa_hard AS walk_17_state_locale_2016_hard,
-ind_walkability.wa_soft AS walk_17_state_locale_2016_soft,
+walk_17_state_locale_2016 = '''ind_walkability.wa_hard AS walk_17_{region}_{locale}_2016_hard,
+ind_walkability.wa_soft AS walk_17_{region}_{locale}_2016_soft,
+'''
 
 # trans_1_vic_melb_2016
 ## < / >= 95% of of residential cadastre with access to bus stop < 400 m OR < 600m of a tram stop OR < 800 m of a train station
-(CASE WHEN SUM(COALESCE(vic_pt.ind_hard,0)) > 0 THEN 1 ELSE 0 END) AS trans_1_vic_melb_2016_hard,
+trans_1_vic_melb_2016 = '''(CASE WHEN SUM(COALESCE(vic_pt.ind_hard,0)) > 0 THEN 1 ELSE 0 END) AS trans_1_vic_melb_2016_hard,
 MAX(COALESCE(vic_pt.ind_soft,0)) AS trans_1_vic_melb_2016_soft,
+'''
+
+
 # trans_2_vic_melb_2016
 ## < / >= 95% of dwellings with access to bus stop < 400 m
-vic_bus.ind_hard AS trans_2_vic_melb_2016_hard,
+trans_2_vic_melb_2016 = '''vic_bus.ind_hard AS trans_2_vic_melb_2016_hard,
 vic_bus.ind_soft AS trans_2_vic_melb_2016_soft,
+'''
+
 
 # NA for moment
   # trans_3_wa_perth_2016
@@ -106,60 +117,85 @@ vic_bus.ind_soft AS trans_2_vic_melb_2016_soft,
 
 # trans_4_qld_bris_2016
 ## < / >= 100% of residential cadastre < 400 metres of an existing or planned public transport stop
-any_pt.ind_hard AS trans_4_qld_bris_2016_hard,
-any_pt.ind_soft AS trans_4_qld_bris_2016_soft,
+trans_4_qld_bris_2016 = '''(CASE WHEN qld_pt.distance <  400  THEN 1
+      WHEN qld_pt.distance >= 400 THEN 0
+      ELSE NULL END) as trans_4_qld_bris_2016_hard,
+ 1-1/(1+exp(-5*(qld_pt.distance-400)/400::double precision)) AS trans_4_qld_bris_2016_soft
+ '''
 
 # trans_5_nsw_syd_2016
 ## < / >= 100% of residential cadastre < 400 m of a bus stop every 30 min OR < 800 m of a train station every 15 min
-(CASE WHEN SUM(COALESCE(nsw_pt_15.ind_hard,0)) > 0 THEN 1 ELSE 0 END) AS trans_5_nsw_syd_2016_hard,
-MAX(COALESCE(nsw_pt_15.ind_soft,0)) AS trans_5_nsw_syd_2016_soft,
-
-(SELECT * FROM od_closest WHERE dest IN (14,15) any_pt_30
+trans_5_nsw_syd_2016 = '''(CASE WHEN SUM(COALESCE(nsw_pt.ind_hard,0)) > 0 THEN 1 ELSE 0 END) AS trans_5_nsw_syd_2016_hard,
+MAX(COALESCE(nsw_pt.ind_soft,0)) AS trans_5_nsw_syd_2016_soft,
+'''
 
 # trans_6_state_locale_2016
-## % of residential dwellings < < 400 m of a public transport stop with a scheduled service at least every 30 minutes between 7am and 7pm on a normal weekday (= a combined measure of proximity and frequency)
-any_pt_30.ind_hard AS trans_6_state_locale_2016_hard
-any_pt_30.ind_soft AS trans_6_state_locale_2016_soft
-
-(SELECT * FROM od_closest WHERE dest = 7) any_pt_30
-
+## % of residential dwellings < 400 m of a public transport stop with a scheduled service at least every 30 minutes between 7am and 7pm on a normal weekday (= a combined measure of proximity and frequency)
+trans_6_state_locale_2016 = '''any_pt_30.ind_hard AS trans_6_{region}_{locale}_2016_hard,
+any_pt_30.ind_soft AS trans_6_{region}_{locale}_2016_soft,
+'''
 
 # pos_1_vic_melb_2016
 ## < / >= 95% of residential dwellings < 400 m of public open space
-vic_pos.ind_hard AS pos_1_vic_melb_2016_hard,
-vic_pos.ind_soft AS pos_1_vic_melb_2016_soft,
+## NOTE : This is identical to national indicator
+pos_400.ind_hard AS pos_1_vic_melb_2016_hard,
+pos_400.ind_soft AS pos_1_vic_melb_2016_soft,
 
-(SELECT * FROM vic_pos WHERE threshold = 400) vic_pos
+# (SELECT * FROM od_pos WHERE threshold = 400) pos_any
 
 # pos_2_wa_perth_2016
 ## < / >= 100% of residential cadastre < 300 m of any public open space
+pos_300.ind_hard AS pos_2_wa_perth_2016_hard,
+pos_300.ind_soft AS pos_2_wa_perth_2016_soft,
+
 
 # pos_3_wa_perth_2016
 ## < / >= 50% of residential dwellings < 400 m of any local park >0.4 to <=1 ha
+pos_3_wa.ind_hard AS pos_3_wa_perth_2016_hard,
+pos_3_wa.ind_soft AS pos_3_wa_perth_2016_soft,
+
 
 # pos_4_wa_perth_2016
 ## < / >= 50% of residential dwellings < 800 m of any neighbourhood park >1 ha - <= 5ha
+pos_4_wa.ind_hard AS pos_4_wa_perth_2016_hard,
+pos_4_wa.ind_soft AS pos_4_wa_perth_2016_soft,
+
 
 # pos_5_wa_perth_2016
 ## < / >= 50% of residential dwellings < 2 km of any district park >5 ha (<=20 ha)
+pos_5_wa.ind_hard AS pos_5_wa_perth_2016_hard,
+pos_5_wa.ind_soft AS pos_5_wa_perth_2016_soft,
 
 # pos_6_qld_bris_2016
 ## </> 90% of residential dwellings < 400 m of a neighbourhood recreation park >0.5 ha
+pos_6_qld.ind_hard AS pos_6_qld_bris_2016_hard,
+pos_6_qld.ind_soft AS pos_6_qld_bris_2016_soft,
 
 # pos_7_qld_bris_2016
 ## </> 90% of residential dwellings < 2.5 km of a district recreation park >5 ha
+pos_7_qld.ind_hard AS pos_7_qld_bris_2016_hard,
+pos_7_qld.ind_soft AS pos_7_qld_bris_2016_soft,
 
 # pos_8_nsw_syd_2016
 ## < / >= 50% of residential dwellings < 400 m of a park >0.5 ha
+pos_8_nsw.ind_hard AS pos_8_nsw_syd_2016_hard,
+pos_8_nsw.ind_soft AS pos_8_nsw_syd_2016_soft,
 
 # pos_9_nsw_syd_2016
 ## < / >= 50% of residential dwellings < 2 km of a park >2 ha
+pos_9_nsw.ind_hard AS pos_9_nsw_syd_2016_hard,
+pos_9_nsw.ind_soft AS pos_9_nsw_syd_2016_soft,
 
 # pos_10_state_locale_2016
 ## % residential dwellings < 400 m of POS
+pos_400.ind_hard AS pos_10_{region}_{locale}_2016_hard,
+pos_400.ind_soft AS pos_10_{region}_{locale}_2016_soft,
 
 # pos_11_state_locale_2016
 ## % residential dwellings < 400 m of POS > 1.5 ha
+pos_400_large.ind_hard AS pos_11_{region}_{locale}_2016_hard,
+pos_400_large.ind_soft AS pos_11_{region}_{locale}_2016_soft,
+
 
 # KNIME 
   # pos_12_state_locale_2016
@@ -187,32 +223,83 @@ vic_pos.ind_soft AS pos_1_vic_melb_2016_soft,
   ## % of employed persons aged 15 and over using private vehicle/s to travel to work
 
 # food_1_state_locale_2016
-## average ratio of green grocers and/or supermarkets to fast food outlets < 1600m average proportion of supermarkets to supermarkets and fast food outlets combined < 3200 m (as per discussion with JR on 13 July 2018)
+## [NOT average ratio of green grocers and/or supermarkets to fast food outlets < 1600m?? as per basecamp discussion with JR  13 July 2018] 
+## average proportion of supermarkets to supermarkets and fast food outlets combined < 3200 m 
+ind_foodratio.supermarket_proportion AS food_1_{region}_{locale}_2016,
 
 # food_2_state_locale_2016
 ## % residential dwellings < 1km of a supermarket
+ind_supermarket1000.ind_sm1000_hard AS food_2_{region}_{locale}_2016_hard,
+ind_supermarket1000.ind_sm1000_soft AS food_2_{region}_{locale}_2016_soft,
 
 # alc_1_state_locale_2016
 ## average number of on-licenses < 400 m
+alc_on.count AS alc_1_{region}_{locale}_2016,
+
 
 # alc_2_state_locale_2016
 ## average number of off-licenses < 800 m
+alc_off.count AS alc_2_{region}_{locale}_2016
 
 
-# create_liveability_ind_list = {'daily_living': dl,
-#                                'walkability': wa,
-#                                'activity_centres': activity,
-#                                'supermarkets_1000': supermarkets_1000,
-#                                'foodratio': foodratio}
-        
-# conn = psycopg2.connect(database=db, user=db_user, password=db_pwd)
-# curs = conn.cursor()
 
-# for ind in create_neighbourhood_ind_list.keys():
-  # print("Creating {} indicator table... ".format(ind)),
-  # curs.execute(create_neighbourhood_ind_list[ind])
-  # conn.commit()
-  # print("Done.")
+
+
+'''
+CREATE TABLE parcel_indicators AS
+SELECT
+p.gnaf_pid               ,
+p.mb_code_20             ,
+p.count_objectid         ,
+p.point_x                ,
+p.point_y                ,
+p.hex_id                 ,
+p.geom                   ,
+abs.mb_code_2016         ,
+abs.mb_category_name_2016,
+abs.dwelling             ,
+abs.person               ,
+abs.sa1_maincode         ,
+abs.sa2_name_2016        ,
+abs.sa3_name_2016        ,
+abs.sa4_name_2016        ,
+abs.gccsa_name           ,
+abs.state_name           ,
+non_abs.ssc_code_2016    ,
+non_abs.ssc_name_2016    ,
+non_abs.lga_code_2016    ,
+non_abs.lga_name_2016    ,
+
+FROM
+ind_activity
+ind_daily_living
+sc_nh1600m
+dd_nh1600m
+ind_walkability
+(SELECT * FROM od_closest WHERE dest IN (9,10,11)) vic_pt
+(SELECT * FROM od_closest WHERE dest = 9) vic_bus
+(SELECT * FROM od_closest WHERE dest = 8) qld_pt
+(SELECT * FROM od_closest WHERE dest IN (14,15) nsw_pt
+(SELECT * FROM od_closest WHERE dest = 7) any_pt_30
+(SELECT * FROM od_pos WHERE threshold = 300) pos_300
+(SELECT * FROM od_pos WHERE threshold = 400) pos_400
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 1.5') pos_400_large
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 0.4 AND area_ha <= 1') pos_3_wa
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 1 AND area_ha <= 5') pos_4_wa
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 5 AND area_ha <= 20') pos_5_wa
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 0.5') pos_6_qld
+(SELECT * FROM od_pos WHERE threshold = 2500 AND query = 'area_ha > 5') pos_7_qld
+(SELECT * FROM od_pos WHERE threshold = 400 AND query = 'area_ha > 0.5') pos_8_nsw
+(SELECT * FROM od_pos WHERE threshold = 2000 AND query = 'area_ha > 2') pos_9_nsw
+ind_foodratio
+ind_supermarket1000
+(SELECT * FROM od_counts WHERE dest = 1) alc_on
+(SELECT * FROM od_counts WHERE dest = 0) alc_off
+'''.format(points_id,locale.lower(),region.lower())
+
+
+
+
 
 # conn.close()
 
