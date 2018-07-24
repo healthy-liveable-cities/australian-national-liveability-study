e66ebbf61cb3b40e926216a4baa6112fb2cb6b59
Carl Higgs <carl.higgs@rmit.edu.au>
2018-07-24 10:10:19 +1000

    merged branch li_adelaide_2016 with master

--- git_merge_branches.bat
+++ git_merge_branches.bat
@@ -1,7 +1,7 @@
 title Merge branches with origin/master
 echo Merge branches with origin/master
 
-FOR %%A  IN (li_adelaide_2016,li_bris_2016,li_canberra_2016,li_darwin_2016,li_hobart_2016,li_melb_2016,li_melb_2016_psma,li_perth_2016,li_syd_2016) DO (
+FOR %%A  IN (li_adelaide_2016,li_bris_2016,li_canberra_2016,li_darwin_2016,li_hobart_2016,li_melb_2016,li_mitchell_2016,li_perth_2016,li_syd_2016) DO (
   git fetch && git checkout %%A
   git pull
   git merge origin/master
