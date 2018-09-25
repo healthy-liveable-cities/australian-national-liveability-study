a3cdd060685765e131814a82c6b3dc4e620f584c
Carl Higgs <carl.higgs@rmit.edu.au>
2018-09-25 12:38:10 +1000

    updated the pos sketch -- re-ordered so updated current code is at top of script

--- pos_sketch.sql
+++ pos_sketch.sql
@@ -7,156 +7,13 @@
 --    -d alburyw_2012 K:/RESEARCH/GIS/Projects/ntnl_li_2018_template/data/21Cities/OSM_Roads/AlburyWodonga/AlburyWodonga_20120912.pbf ^
 --    --hstore --style ./osm2pgsql/default.style
   
--- Create open space table (broad concept) 
-CREATE TABLE open_space AS 
-SELECT * FROM planet_osm_polygon p 
-WHERE (p.leisure IS NOT NULL 
-    OR p.natural IS NOT NULL 
-    OR p.sport IS NOT NULL  
-    OR p.landuse IN ('forest','grass','greenfield','meadow','recreation ground','village green')) 
-  AND (p.access IS NULL 
-    OR p.access NOT IN('no','private'));
-
--- Create unique POS id 
-ALTER TABLE open_space ADD COLUMN pos_id SERIAL PRIMARY KEY;         
---Add geometry in spatial reference for project 
-ALTER TABLE open_space ADD COLUMN geom geometry; 
-UPDATE open_space SET geom = ST_Transform(way,7845); 
-
--- Create variable for park size 
-ALTER TABLE open_space ADD COLUMN area_ha double precision; 
-UPDATE open_space SET area_ha = ST_Area(geom)/10000.0;
-
--- Create a linestring pos table 
-CREATE TABLE pos_line AS 
-SELECT pos_id, ST_Length(geom)::numeric AS length, geom    
-FROM (SELECT pos_id, ST_ExteriorRing(geom) AS geom FROM open_space) t;
-
--- Generate a point every 20m along a park outlines: 
-DROP TABLE pos_nodes; 
-CREATE TABLE pos_nodes AS 
- WITH pos AS 
- (SELECT pos_id, 
-         length, 
-         generate_series(0,1,20/length) AS fraction, 
-         geom FROM pos_line) 
-SELECT pos_id,
-       row_number() over(PARTITION BY pos_id) AS node, 
-       ST_LineInterpolatePoint(geom, fraction)  AS geom 
-FROM pos;
-
--- Create table of points within 20m of lines (should be your road network) 
--- Distinct is used to avoid redundant duplication of points where they are within 20m of multiple roads 
-CREATE TABLE pos_nodes_20m_line AS 
-SELECT DISTINCT p.* 
-FROM pos_nodes p, 
-     (SELECT ST_Transform(way,7845) geom FROM planet_osm_line) l 
-WHERE ST_DWithin(p.geom ,l.geom,20);
 
 
--- Create table of points within 50m of lines (should be your road network) 
--- Distinct is used to avoid redundant duplication of points where they are within 50m of multiple roads 
-CREATE TABLE pos_nodes_50m_line AS 
-SELECT DISTINCT p.* 
-FROM pos_nodes p, 
-     (SELECT ST_Transform(way,7845) geom FROM planet_osm_line) l 
-WHERE ST_DWithin(p.geom ,l.geom,50);
-
-      
--- Create table of pos OD matrix results
-CREATE TABLE pos_od 
-(
-participant_id INT, 
-pos_id INT, 
-node INT, 
-distance INT, 
-PRIMARY KEY (participant_id,pos_id) 
-);
-
-INSERT INTO pos_od (participant_id, pos_id, node, distance) 
-SELECT DISTINCT ON (participant_id, pos_id) participant_id, pos_id, node, min(distance) 
-FROM  ( 
-   VALUES 
-   (15151, 32, 23, 567), 
-   (15151, 32, 1, 530), 
-   (15151, 32, 3, 600), 
-   (15151, 27, 9, 34), 
-   (15152, 27, 13, 11), 
-   (15152, 3, 21, 132),  
-   (15152, 27, 21, 1332), 
-   (15153, 27, 32, 198) 
-   ) v(participant_id, pos_id, node, distance) 
-   GROUP BY participant_id,pos_id,node 
-    ON CONFLICT (participant_id,pos_id) 
-       DO UPDATE 
-          SET node = EXCLUDED.node, 
-              distance = EXCLUDED.distance 
-           WHERE pos_od.distance > EXCLUDED.distance;
-      
--- Associate participants with list of parks 
-DROP TABLE IF EXISTS pos_jsonb_agg; 
-CREATE TABLE pos_jsonb_agg AS 
-SELECT p.participant_id, 
-       jsonb_agg(jsonb_strip_nulls(to_jsonb( 
-           (SELECT d FROM 
-               (SELECT 
-                  p.distance, 
-                  a.area_ha, 
-                  a.access,  
-                  a.landuse, 
-                  a.leisure, 
-                  a.natural, 
-                  a.sport, 
-                  a.waterway, 
-                  a.wood) d)))) AS attributes 
-FROM pos_od p 
-LEFT JOIN open_space a ON p.pos_id = a.pos_id 
-GROUP BY participant_id;   
-
--- Select only those items in the list which meet numeric criteria: 
-SELECT participant_id, jsonb_agg(obj) AS attributes 
-FROM pos_jsonb_agg, jsonb_array_elements(attributes) obj 
-WHERE obj->'area_ha' > '0.5' 
-GROUP BY participant_id; 
-  
--- Select count for each participant matching criteria 
-SELECT participant_id, jsonb_array_length(jsonb_agg(obj)) AS count 
-FROM pos_jsonb_agg, jsonb_array_elements(attributes) obj 
-WHERE obj->'area_ha' > '0.5'  
-GROUP BY participant_id; 
-   
--- Select only those items in the list which meet multiple criteria 
-SELECT participant_id, jsonb_agg(obj) AS attributes 
-FROM pos_jsonb_agg, jsonb_array_elements(attributes) obj 
-WHERE obj->'area_ha' > '0.2'  AND obj->'distance' < '100'  
-GROUP BY participant_id; 
-
--- Select count and return attributes for each participant matching multiple criteria
--- (mixed numeric and qualitative)
-SELECT participant_id, 
-       jsonb_array_length(jsonb_agg(obj)) AS count, 
-       jsonb_agg(obj) AS attributes 
-FROM pos_jsonb_agg, jsonb_array_elements(attributes) obj 
-WHERE obj->  'leisure' = '"park"' AND obj->'distance' < '100' 
-GROUP BY participant_id; 
-
--- return attributes in a non-json form, without grouping or filtering 
-SELECT participant_id, 
-       obj->'area_ha' AS area_ha,     
-       obj->'distance' AS distance,    
-       obj->'leisure' AS leisure 
-FROM pos_jsonb_agg, jsonb_array_elements(attributes) obj; 
-
-
-
-
-
---- Part 2
---- Refined version of additional operations
+-- Add geom column to polygon table, appropriately transformed to project spatial reference system
 ALTER TABLE planet_osm_polygon ADD COLUMN geom geometry; 
 UPDATE planet_osm_polygon SET geom = ST_Transform(way,7845); 
 
-
+-- Create an 'Open Space' table
 DROP TABLE open_space;
 CREATE TABLE open_space AS 
 SELECT * FROM planet_osm_polygon p 
@@ -274,11 +131,6 @@ UPDATE open_space SET linear_feature = TRUE
 WHERE amal_to_area_ratio > 140 
   AND area_ha > 0.5 
   AND medial_axis_length > 300;
-
-      
-      
-      
-      
 
 -- So, we have hints
 -- We know that 
@@ -437,24 +289,24 @@ GROUP BY gid,aos_ha_school,numgeom,aos_ha,attributes;
 DROP TABLE pos_line4;
 CREATE TABLE pos_line4 AS 
 WITH school_bounds AS
-   (SELECT gid, ST_SetSRID(st_astext((ST_Dump(school_geom)).geom),7845) AS school_geom  FROM open_space_areas2)
-SELECT gid, ST_Length(school_geom)::numeric AS length, school_geom    
-FROM (SELECT gid, ST_ExteriorRing(school_geom) AS school_geom FROM school_bounds) t;
+   (SELECT gid, ST_SetSRID(st_astext((ST_Dump(geom_w_schools)).geom),7845) AS geom_w_schools  FROM open_space_areas2)
+SELECT gid, ST_Length(geom_w_schools)::numeric AS length, geom_w_schools    
+FROM (SELECT gid, ST_ExteriorRing(geom_w_schools) AS geom_w_schools FROM school_bounds) t;
 
 -- Generate a point every 20m along a park outlines: 
-DROP TABLE pos_nodes4; 
-CREATE TABLE pos_nodes4 AS 
+DROP TABLE pos_nodes; 
+CREATE TABLE pos_nodes AS 
  WITH pos AS 
  (SELECT gid, 
          length, 
          generate_series(0,1,20/length) AS fraction, 
-         school_geom FROM pos_line4) 
+         geom_w_schools FROM pos_line4) 
 SELECT gid,
        row_number() over(PARTITION BY gid) AS node, 
-       ST_LineInterpolatePoint(school_geom, fraction)  AS school_geom 
+       ST_LineInterpolatePoint(geom_w_schools, fraction)  AS geom_w_schools 
 FROM pos;
 
-CREATE INDEX pos_nodes4_idx ON pos_nodes4 USING GIST (school_geom);
+CREATE INDEX pos_nodes_idx ON pos_nodes USING GIST (geom_w_schools);
 
 -- Preparation of reference line feature to act like roads
 ALTER TABLE planet_osm_line ADD COLUMN geom geometry; 
@@ -462,28 +314,185 @@ UPDATE planet_osm_line SET geom = ST_Transform(way,7845);
 
 -- Create table of points within 20m of lines (should be your road network) 
 -- Distinct is used to avoid redundant duplication of points where they are within 20m of multiple roads 
-CREATE TABLE pos_nodes_20m_line4 AS 
+CREATE TABLE pos_nodes_20m_line AS 
 SELECT DISTINCT n.* 
 FROM pos_nodes4 n, 
      planet_osm_line l
-WHERE ST_DWithin(n.school_geom ,l.geom,20)
+WHERE ST_DWithin(n.geom_w_schools ,l.geom,20)
 AND l.highway IS NOT NULL;
 
 -- Create table of points within 30m of lines (should be your road network) 
 -- Distinct is used to avoid redundant duplication of points where they are within 20m of multiple roads 
-CREATE TABLE pos_nodes_30m_line4 AS 
+CREATE TABLE pos_nodes_30m_line AS 
 SELECT DISTINCT n.* 
 FROM pos_nodes4 n, 
      planet_osm_line l
-WHERE ST_DWithin(n.school_geom ,l.geom,30)
+WHERE ST_DWithin(n.geom_w_schools ,l.geom,30)
 AND l.highway IS NOT NULL;
 
 -- Create table of points within 50m of lines (should be your road network) 
 -- Distinct is used to avoid redundant duplication of points where they are within 20m of multiple roads 
-CREATE TABLE pos_nodes_50m_line4 AS 
+CREATE TABLE pos_nodes_50m_line AS 
 SELECT DISTINCT n.* 
 FROM pos_nodes4 n, 
      planet_osm_line l
-WHERE ST_DWithin(n.school_geom ,l.geom,50)
+WHERE ST_DWithin(n.geom_w_schools ,l.geom,50)
 AND l.highway IS NOT NULL;
+
+
+
+
+----- OLD CODE; still useful stuff at the end for selecting features using json
+-----  In the below approach, the OS features are aggregated based on results of an OD analysis;
+---------- The strength of this approach is that access to each individual feature is evaluated
+---------- The weakness is, where features are overlapping (e.g. a park with an oval and cricket pitch, all nested) 
+---------- these will be double counted.  Also, some internal features which would otherwise be accessed via a park
+---------- may be inappropriately snapped to network, or not at all.
+-----  In the above, we create AOS features on whcih to conduct OD analysis
+---------- The strength is that where individual features are not accessible to network due to being
+---------- contained or otherwise strongly associated with a broader area of open space,
+---------- access is measured to the broader area, which retains attributes of its containing feature
+---------- allowing it to be queried
+
+-- Create open space table 
+CREATE TABLE open_space AS 
+SELECT * FROM planet_osm_polygon p 
+WHERE (p.leisure IS NOT NULL 
+    OR p.natural IS NOT NULL 
+    OR p.sport IS NOT NULL  
+    OR p.landuse IN ('forest','grass','greenfield','meadow','recreation ground','village green')) 
+  AND (p.access IS NULL 
+    OR p.access NOT IN('no','private'));
+
+-- Create unique POS id 
+ALTER TABLE open_space ADD COLUMN pos_id SERIAL PRIMARY KEY;         
+--Add geometry in spatial reference for project 
+ALTER TABLE open_space ADD COLUMN geom geometry; 
+UPDATE open_space SET geom = ST_Transform(way,7845); 
+
+-- Create variable for park size 
+ALTER TABLE open_space ADD COLUMN area_ha double precision; 
+UPDATE open_space SET area_ha = ST_Area(geom)/10000.0;
+
+-- Create a linestring pos table 
+CREATE TABLE pos_line AS 
+SELECT pos_id, ST_Length(geom)::numeric AS length, geom    
+FROM (SELECT pos_id, ST_ExteriorRing(geom) AS geom FROM open_space) t;
+
+-- Generate a point every 20m along a park outlines: 
+DROP TABLE pos_nodes; 
+CREATE TABLE pos_nodes AS 
+ WITH pos AS 
+ (SELECT pos_id, 
+         length, 
+         generate_series(0,1,20/length) AS fraction, 
+         geom FROM pos_line) 
+SELECT pos_id,
+       row_number() over(PARTITION BY pos_id) AS node, 
+       ST_LineInterpolatePoint(geom, fraction)  AS geom 
+FROM pos;
+
+-- Create table of points within 20m of lines (should be your road network) 
+-- Distinct is used to avoid redundant duplication of points where they are within 20m of multiple roads 
+CREATE TABLE pos_nodes_20m_line AS 
+SELECT DISTINCT p.* 
+FROM pos_nodes p, 
+     (SELECT ST_Transform(way,7845) geom FROM planet_osm_line) l 
+WHERE ST_DWithin(p.geom ,l.geom,20);
+
+
+-- Create table of points within 50m of lines (should be your road network) 
+-- Distinct is used to avoid redundant duplication of points where they are within 50m of multiple roads 
+CREATE TABLE pos_nodes_50m_line AS 
+SELECT DISTINCT p.* 
+FROM pos_nodes p, 
+     (SELECT ST_Transform(way,7845) geom FROM planet_osm_line) l 
+WHERE ST_DWithin(p.geom ,l.geom,50);
+
+      
+-- Create table of pos OD matrix results
+CREATE TABLE pos_od 
+(
+participant_id INT, 
+pos_id INT, 
+node INT, 
+distance INT, 
+PRIMARY KEY (participant_id,pos_id) 
+);
+
+INSERT INTO pos_od (participant_id, pos_id, node, distance) 
+SELECT DISTINCT ON (participant_id, pos_id) participant_id, pos_id, node, min(distance) 
+FROM  ( 
+   VALUES 
+   (15151, 32, 23, 567), 
+   (15151, 32, 1, 530), 
+   (15151, 32, 3, 600), 
+   (15151, 27, 9, 34), 
+   (15152, 27, 13, 11), 
+   (15152, 3, 21, 132),  
+   (15152, 27, 21, 1332), 
+   (15153, 27, 32, 198) 
+   ) v(participant_id, pos_id, node, distance) 
+   GROUP BY participant_id,pos_id,node 
+    ON CONFLICT (participant_id,pos_id) 
+       DO UPDATE 
+          SET node = EXCLUDED.node, 
+              distance = EXCLUDED.distance 
+           WHERE pos_od.distance > EXCLUDED.distance;
+      
+-- Associate participants with list of parks 
+DROP TABLE IF EXISTS open_space_areas; 
+CREATE TABLE open_space_areas AS 
+SELECT p.participant_id, 
+       jsonb_agg(jsonb_strip_nulls(to_jsonb( 
+           (SELECT d FROM 
+               (SELECT 
+                  p.distance, 
+                  a.area_ha, 
+                  a.access,  
+                  a.landuse, 
+                  a.leisure, 
+                  a.natural, 
+                  a.sport, 
+                  a.waterway, 
+                  a.wood) d)))) AS attributes 
+FROM pos_od p 
+LEFT JOIN open_space a ON p.pos_id = a.pos_id 
+GROUP BY participant_id;   
+
+-- Select only those items in the list which meet numeric criteria: 
+SELECT participant_id, jsonb_agg(obj) AS attributes 
+FROM open_space_areas, jsonb_array_elements(attributes) obj 
+WHERE obj->'area_ha' > '0.5' 
+GROUP BY participant_id; 
+  
+-- Select count for each participant matching criteria 
+SELECT participant_id, jsonb_array_length(jsonb_agg(obj)) AS count 
+FROM open_space_areas, jsonb_array_elements(attributes) obj 
+WHERE obj->'area_ha' > '0.5'  
+GROUP BY participant_id; 
+   
+-- Select only those items in the list which meet multiple criteria 
+SELECT participant_id, jsonb_agg(obj) AS attributes 
+FROM open_space_areas, jsonb_array_elements(attributes) obj 
+WHERE obj->'area_ha' > '0.2'  AND obj->'distance' < '100'  
+GROUP BY participant_id; 
+
+-- Select count and return attributes for each participant matching multiple criteria
+-- (mixed numeric and qualitative)
+SELECT participant_id, 
+       jsonb_array_length(jsonb_agg(obj)) AS count, 
+       jsonb_agg(obj) AS attributes 
+FROM open_space_areas, jsonb_array_elements(attributes) obj 
+WHERE obj->  'leisure' = '"park"' AND obj->'distance' < '100' 
+GROUP BY participant_id; 
+
+-- return attributes in a non-json form, without grouping or filtering 
+SELECT participant_id, 
+       obj->'area_ha' AS area_ha,     
+       obj->'distance' AS distance,    
+       obj->'leisure' AS leisure 
+FROM open_space_areas, jsonb_array_elements(attributes) obj; 
+
+
 
