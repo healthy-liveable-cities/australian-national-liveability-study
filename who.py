109e31bffdb433ad1c8963d61068a9481f466f57
Carl Higgs <carl.higgs@rmit.edu.au>
2018-10-12 15:45:30 +1100

    update

--- who.py
+++ who.py
@@ -17,4 +17,6 @@ df_studyregion = pandas.read_excel(xls, 'study_regions',index_col=1)
 responsible = df_studyregion['responsible']
 print(df_about.iloc[0].reset_index()['index'].to_string(index=False).encode('utf'))
 print(responsible.reset_index().sort_values(by=['responsible','locale']).to_string(index=False))
-
+print("\n Some useful shortcut batch scripts:")
+for person in [x.encode('utf') for x in responsible.unique().tolist() if type(x) is not float]:
+  print("{person}: process_scripts_01_06.bat {regions}".format(person = person, regions = ' '.join([x .encode('utf') for x in responsible[responsible==person].reset_index()['locale'].sort_values().tolist()])))
