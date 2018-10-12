bdcfa173735eca433ec32bf4d3eb243ec665bf99
Carl Higgs <carl.higgs@rmit.edu.au>
2018-10-12 11:14:52 +1100

    updates -- now you can write 'python who.py' to find out what you need to do

--- who.py
+++ who.py
@@ -4,6 +4,16 @@
 # Date:    20181009
 
 # Import custom variables for National Liveability indicator process
-from config_ntnl_li_process import pandas,responsible
+# import modules
+import os
+import sys
+import pandas
 
-print(responsible.reset_index().sort(['responsible','locale']).to_string(index=False))
+# Load settings from ind_study_region_matrix.xlsx
+xls = pandas.ExcelFile(os.path.join(sys.path[0],'ind_study_region_matrix.xlsx'))
+df_about = pandas.read_excel(xls, 'about')
+df_studyregion = pandas.read_excel(xls, 'study_regions',index_col=1)
+
+responsible = df_studyregion['responsible']
+print(df_about.iloc[0].reset_index()['index'].to_string(index=False).encode('utf'))
+print(responsible.reset_index().sort_values(by=['responsible','locale']).to_string(index=False))
