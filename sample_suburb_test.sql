c361b60ac187a6ad99606428c8611f502ea700c1
Carl Higgs <carl.higgs@rmit.edu.au>
2018-10-17 11:08:50 +1100

    updated and formalised the sample_suburb_test.sql function code

--- sample_suburb_test.sql
+++ sample_suburb_test.sql
@@ -1,3 +1,98 @@
+-- Actual functions
+DROP TABLE IF EXISTS sample_suburb_20; CREATE TABLE sample_suburb_20 AS SELECT * FROM boundaries_ssc_bris_2016 TABLESAMPLE BERNOULLI (100*50/(SELECT COUNT(*) FROM boundaries_ssc_bris_2016)) LIMIT 20;
+
+CREATE OR REPLACE FUNCTION suburb_summary(text) RETURNS TABLE(key text, distinct_values text) AS 
+    $$
+    WITH 
+    area AS (SELECT o.* 
+             FROM open_space_areas o,
+                  (SELECT ST_Transform(geom,7845) AS geom FROM sample_suburb_20 WHERE ssc_name_2016 = $1) s 
+             WHERE o.geom_w_schools && s.geom AND ST_Intersects(o.geom_w_schools,s.geom)),
+    raw_num AS (SELECT aos_id,
+                       numgeom, 
+                       (jsonb_array_elements(attributes)->>'area_ha')::numeric AS os_ha, 
+                       aos_ha::numeric, 
+                       aos_ha_total::numeric,
+                       aos_ha_water::numeric,
+                       water_percent::numeric,
+                       school_os_percent::numeric 
+                FROM area),
+    d AS (SELECT aos_id, 
+                 numgeom, 
+                 ROUND(AVG(os_ha),2) || ' (' || ROUND(MIN(os_ha),2) || ' - ' || ROUND(MAX(os_ha),2) || ')' AS mean_range_os_ha, 
+                 aos_ha, 
+                 aos_ha_total,
+                 aos_ha_water,
+                 water_percent,             
+                 school_os_percent             
+          FROM raw_num 
+          GROUP BY aos_id, numgeom, aos_ha, aos_ha_total,aos_ha_water,water_percent,school_os_percent),
+    aos_averages AS (SELECT ROUND(AVG(d.aos_ha),2)  || ' (' || ROUND(MIN(d.aos_ha),2)  || ' - ' || ROUND(MAX(d.aos_ha),2)  || ')' AS mean_range_aos_ha,
+                            ROUND(AVG(d.aos_ha_total),2)  || ' (' || ROUND(MIN(d.aos_ha_total),2)  || ' - ' || ROUND(MAX(d.aos_ha_total),2)  || ')' AS mean_range_aos_ha_total,
+                            ROUND(AVG(d.aos_ha_water),2)  || ' (' || ROUND(MIN(d.aos_ha_water),2)  || ' - ' || ROUND(MAX(d.aos_ha_water),2)  || ')' AS mean_range_aos_ha_water,
+                            ROUND(AVG(d.numgeom),2) || ' (' || ROUND(MIN(d.numgeom),2) || ' - ' || ROUND(MAX(d.numgeom),2) || ')' AS mean_range_numgeom,
+                            ROUND(AVG(r.os_ha),2) || ' (' || ROUND(MIN(r.os_ha),2) || ' - ' || ROUND(MAX(r.os_ha),2) || ')' AS mean_range_os_ha,
+                            COUNT(DISTINCT(d.aos_id)) AS aos_count,
+                            ROUND(AVG(d.water_percent),2)  || ' (' || ROUND(MIN(d.water_percent),2)  || ' - ' || ROUND(MAX(d.water_percent),2)  || ')' AS mean_range_water_percent,
+                            ROUND(AVG(d.school_os_percent),2)  || ' (' || ROUND(MIN(d.school_os_percent),2)  || ' - ' || ROUND(MAX(d.school_os_percent),2)  || ')' AS mean_range_school_os_percent
+                FROM d,raw_num r),
+    num AS (SELECT jsonb_agg(jsonb_strip_nulls(to_jsonb(aos_averages))) AS numeric_summary FROM d,aos_averages),
+    str AS (SELECT to_jsonb(jsonb_each(jsonb_array_elements(attributes || numeric_summary ) - 'acceptable_linear_feature' - 'medial_axis_length' - 'area_ha' - 'num_symdiff_convhull_geoms' - 'os_id' - 'roundness'  )) att FROM area,num)
+    SELECT att->>'key' AS key,string_agg(DISTINCT(att->>'value'),', ') AS distinct_values FROM str GROUP BY key;
+    $$
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
+
+    
+SELECT ssc_name_2016 FROM sample_suburb_20 ORDER BY (ssc_name_2016);
+--     ssc_name_2016
+-- ---------------------
+--  Amity
+--  Blantyre
+--  Boondall
+--  Browns Plains (Qld)
+--  Bryden
+--  Caboolture South
+--  Calvert (Qld)
+--  Cannon Hill
+--  Coochiemudlo Island
+--  Daisy Hill (Qld)
+--  Deebing Heights
+--  Dutton Park
+--  East Brisbane
+--  Forestdale
+--  Glamorgan Vale
+--  Grandchester
+--  Holmview
+--  Merryvale
+--  Veresdale Scrub
+--  Vernor
+-- (20 rows)
+
+SELECT * FROM suburb_summary('Amity'); 
+SELECT * FROM suburb_summary('Blantyre'); 
+SELECT * FROM suburb_summary('Boondall'); 
+SELECT * FROM suburb_summary('Browns Plains (Qld)'); 
+SELECT * FROM suburb_summary('Bryden'); 
+SELECT * FROM suburb_summary('Caboolture South'); 
+SELECT * FROM suburb_summary('Calvert (Qld)'); 
+SELECT * FROM suburb_summary('Cannon Hill'); 
+SELECT * FROM suburb_summary('Coochiemudlo Island'); 
+SELECT * FROM suburb_summary('Daisy Hill (Qld)'); 
+SELECT * FROM suburb_summary('Deebing Heights'); 
+SELECT * FROM suburb_summary('Dutton Park'); 
+SELECT * FROM suburb_summary('East Brisbane'); 
+SELECT * FROM suburb_summary('Forestdale'); 
+SELECT * FROM suburb_summary('Glamorgan Vale'); 
+SELECT * FROM suburb_summary('Grandchester'); 
+SELECT * FROM suburb_summary('Holmview'); 
+SELECT * FROM suburb_summary('Merryvale'); 
+SELECT * FROM suburb_summary('Veresdale Scrub'); 
+SELECT * FROM suburb_summary('Vernor'); 
+    
+    
+-- PREVIOUS SKETCH NOTES (prior to 15 October 2018
 
 SELECT * 
 FROM
