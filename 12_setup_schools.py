4fdde11c02edb5948cb6f03fe810703d116c23be
Carl Higgs <carl.higgs@rmit.edu.au>
2018-10-18 16:12:04 +1100

    updated the set up schools script, but more work is needed to integrate the schools not matched to an osm polygon

--- 12_setup_schools.py
+++ 12_setup_schools.py
@@ -58,7 +58,8 @@ aos_setup = ['''
 DROP TABLE IF EXISTS osm_schools;
 CREATE TABLE osm_schools AS 
 SELECT * FROM {osm_prefix}_polygon p 
-WHERE p.amenity IN ('school','college') OR p.landuse IN ('school');
+WHERE p.amenity IN ('school','college','university') 
+   OR p.landuse IN ('school','college','university');
 '''.format(osm_prefix = osm_prefix),
 '''
 ALTER TABLE osm_schools ADD COLUMN area_ha double precision; 
@@ -67,30 +68,56 @@ ALTER TABLE osm_schools ADD COLUMN is_school boolean;
 UPDATE osm_schools SET is_school = TRUE;
 ''',
 '''
-ALTER TABLE osm_schools ADD COLUMN ext_school_id int; 
-ALTER TABLE osm_schools ADD COLUMN ext_school_dist int; 
+ALTER TABLE osm_schools ADD COLUMN school_tags jsonb;       
+ALTER TABLE osm_schools ADD COLUMN school_count int;       
+UPDATE osm_schools o SET school_count = 0;
 ''',
 '''
-UPDATE osm_schools o 
-   SET ext_school_id = {school_id}::int, ext_school_dist = 0
-  FROM {ext_schools} s
- WHERE ST_Intersects(s.geom, o.geom);
+UPDATE osm_schools t1 
+   SET school_tags = jsonb(t2.school_tags), school_count = t1.school_count + t2.school_count
+FROM (-- here we aggregate and count the sets of school tags associated with school polygons
+      -- by virtue of those being for their associated schools their closest match within 100m.
+      -- School points more than 100m from a polygon will remain unmatched.
+      -- The basis for an allowed distance of 100m is that some points may be located at the driveway
+      -- from which a school is accessed --- for some schools, this may be several hundred metres 
+      -- from a road.
+      -- We haven't restricted it to polygons with null as it may be that 
+      -- previously associated polgon-school groupings (by virtue of intersection, 0 distance)
+      -- may have a further school point coincidentally (mis?)geocoded outside the bounds of the 
+      -- OSM school polygon.  If there is no better matchin school polygon, it may be best to assume 
+      -- that the point should best be associated with the other schools.
+      SELECT osm_id,
+             count(*) AS school_count,
+             jsonb_agg(to_jsonb(t) - 'osm_id'::text  - 'matched_school'::text  - 'school_tags'::text)  AS school_tags
+      FROM (SELECT DISTINCT ON ({school_id})
+            CASE 
+              WHEN ST_Intersects(schools.geom, osm.geom) THEN 0
+              ELSE ST_Distance(schools.geom, ST_ExteriorRing(osm.geom))::int  
+            END AS dist,
+            schools.*, 
+            osm.osm_id,
+            osm.school_tags
+            FROM (SELECT a.*, o.matched_school FROM {ext_schools} a 
+                  LEFT JOIN 
+                  (SELECT (jsonb_array_elements(school_tags)->>'{school_id}') AS matched_school FROM osm_schools) o 
+                  ON a.{school_id}::text = o.matched_school,{studyregion} s 
+                  WHERE ST_Intersects(a.geom,s.geom) AND matched_school IS NULL) schools,
+                  osm_schools osm
+      WHERE ST_DWithin(schools.geom, ST_ExteriorRing(osm.geom), 150) OR ST_Intersects(schools.geom, osm.geom)
+      ORDER BY {school_id},ST_Distance(schools.geom, ST_ExteriorRing(osm.geom))) t
+      GROUP BY osm_id) t2
+ WHERE t1.osm_id = t2.osm_id;
+-- -- School match table - commented out for now, but useful for cross checking
+-- DROP TABLE IF EXISTS school_matches;    
+-- CREATE TABLE school_matches AS 
+-- SELECT a.*, o.matched_school FROM {ext_schools} a 
+--                   LEFT JOIN 
+--                   (SELECT (jsonb_array_elements(school_tags)->>'{school_id}') AS matched_school FROM osm_schools) o 
+--                   ON a.{school_id}::text = o.matched_school,{studyregion} s 
+--                   WHERE ST_Intersects(a.geom,s.geom);
 '''.format(ext_schools =  os.path.basename(school_destinations),
-           school_id = school_id.lower()),
-'''
-UPDATE osm_schools o 
-   SET ext_school_id = {school_id}::int, ext_school_dist = dist::int
-  FROM (SELECT DISTINCT ON ({school_id}) 
-          {school_id}, 
-           s.osm_id, 
-           ST_Distance(s.geom, o.geom)  as dist
-        FROM {ext_schools} AS o , osm_schools AS s  
-        WHERE ST_DWithin(s.geom, o.geom, 500) 
-        ORDER BY {school_id}, s.osm_id, ST_Distance(s.geom, o.geom)) t
- WHERE o.osm_id = t.osm_id 
-   AND o.ext_school_dist IS NULL;
-'''.format(ext_schools =  os.path.basename(school_destinations),
-           school_id = school_id.lower())
+           school_id = school_id.lower(),
+           studyregion = buffered_study_region)
 ]
 
 
