397d1ec98d2fff7844b17dd7b2ffa8c53ba4e164
Carl Higgs <carl.higgs@rmit.edu.au>
2018-10-19 16:47:15 +1100

    renamed scripts, making room for os co-location script

--- process_scripts_16_18_nh_measures.bat
+++ process_scripts_16_18_nh_measures.bat
@@ -2,9 +2,9 @@ title Process neighbourhood measures scripts for study regions
 echo Process neighbourhood measures scripts for study regions
 
 FOR %%A  IN (%*) DO (
-  python 15_area_linkage_tables.py %%A
-  python 16_dwelling_density.py %%A
-  python 17_street_connectivity.py %%A
+  python 16_area_linkage_tables.py %%A
+  python 17_dwelling_density.py %%A
+  python 18_street_connectivity.py %%A
 )
 
 @pause
