bc33d1613fb844cc5df32311b2a5a3beb53091d9
Carl Higgs <carl.higgs@rmit.edu.au>
2018-12-12 12:05:02 +1100

    udpated the pos and pt summary script

--- summarise_pt_and_pos.py
+++ summarise_pt_and_pos.py
@@ -12,15 +12,26 @@ from config_ntnl_li_process import *
 conn = psycopg2.connect(dbname=db, user=db_user, password=db_pwd)
 curs = conn.cursor()  
 
-sql = [['''SELECT (SUM(ST_Area(geom_public))+SUM(ST_Area(geom_water)))/10000 AS area_ha FROM open_space_areas;''','POS area','Ha']  ,
+sql = [['''SELECT ROUND(((SUM(ST_Area(geom_public))+SUM(ST_Area(geom_water)))/10000.0)::numeric,2) AS area_ha FROM open_space_areas;''','POS area','Ha']  ,
        ['''SELECT COUNT(*) FROM study_destinations WHERE dest_class = 'gtfs_2018_stop_30_mins_final';''','PT (30 min freq)','stops'],
        ['''SELECT COUNT(*) FROM study_destinations WHERE dest_class = 'gtfs_2018_stops';''',             'PT (any)','stops'],
        ['''SELECT COUNT(*) FROM study_destinations WHERE dest_class = 'gtfs_2018_stops_bus';''',         'PT (bus)','stops'],
        ['''SELECT COUNT(*) FROM study_destinations WHERE dest_class = 'gtfs_2018_stops_train';''',       'PT (train)','stops'],
        ['''SELECT COUNT(*) FROM study_destinations WHERE dest_class = 'gtfs_2018_stops_ferry';''',       'PT (ferry)','stops']]
-
+   
+values = []
 for query in sql:
   curs.execute(query[0])
-  print("{}: {} {}".format(query[1],list(curs)[0][0],query[2]))
+  values.append(float(list(curs)[0][0]))
+
+print("Pretty:")  
+print('''{:>16} {:>16} {:>16} {:>16} {:>16} {:>16}'''.format('POS area (Ha)','PT (30 min freq)','PT (any)','PT (bus)','PT (train)','PT (ferry)'))
+print('''{:16} {:16} {:16} {:16} {:16} {:16}'''.format(*values))
+    
+print("\nComma seperated:")        
+print('''{},{},{},{}{},{}'''.format('POS area (Ha)','PT (30 min freq)','PT (any)','PT (bus)','PT (train)','PT (ferry)'))
+print('''{},{},{},{} {},{}'''.format(*values))
 
 conn.close()
+
+
