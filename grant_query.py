647d8acac1a1f872a4065cfed07d572d28987b7a
Carl Higgs <carl.higgs@rmit.edu.au>
2018-12-18 11:03:48 +1100

    added in a script to print grant query

---
+++ grant_query.py
@@ -0,0 +1,10 @@
+# Script:  grant_query.py
+# Purpose: This script prints out the grant query which must on occassions be run manually
+#
+# Author:  Carl Higgs
+# Date:    20181218
+
+# Import custom variables for National Liveability indicator process
+from config_ntnl_li_process import *
+
+print(grant_query)
