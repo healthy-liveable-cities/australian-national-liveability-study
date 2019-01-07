ebb06a9fad58a4baf90b39e32be8a6d1f659aa55
Carl Higgs <carl.higgs@rmit.edu.au>
2019-01-07 12:04:56 +1100

    corrected parcel exclusion scripts; these now create a persisting tabler of network islands

--- 20_parcel_exclusion_preliminary_alt.py
+++ 20_parcel_exclusion_preliminary_alt.py
@@ -158,7 +158,8 @@ if island_reviewed is False:
 if island_reviewed is True:
   print(" [study region is flagged as having been reviewed] ")
 print("(check table 'network_islands' to see if any large non-main network islands are legitimate islands;\nif so, they can be whitelisted in the project configuration file)\nSummary of network islands:")
-summary = pandas.read_sql_query('''
+
+network_islands = '''
 --Create a geometry table for network island clusters 
 DROP TABLE IF EXISTS network_islands; 
 CREATE TABLE network_islands AS 
@@ -180,7 +181,11 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO arc_sde;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO arc_sde; 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO python; 
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO python; 
+'''
+curs.execute(network_islands)
+conn.commit()
 
+summary = pandas.read_sql_query('''
 --Summarise length in descending order 
 SELECT ROUND(length::numeric,0)::int AS length_metres from network_islands ORDER BY length DESC;  
 ''',con=engine) 
