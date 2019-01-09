1735d9d9e94391711037c357278264fa4548609f
Carl Higgs <carl.higgs@rmit.edu.au>
2019-01-10 08:22:03 +1100

    added in batch file for ncpf pos v2 script

---
+++ batch_ncpf_pos_v2.bat
@@ -0,0 +1,11 @@
+title Recompiled destinations and summarise for study regions
+echo  Recompiled destinations and summarise for study regions
+echo  **NOTE: Assumes you have a current version of destinations database**
+
+FOR %%A  IN (%*) DO (
+  python 
+python ncpf_od_closest_pos_v2.py %%A any
+python ncpf_od_closest_pos_v2.py %%A large
+)
+
+@pause
