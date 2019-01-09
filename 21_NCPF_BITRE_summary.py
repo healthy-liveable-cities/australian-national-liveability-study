02311290aa671d9e6e70419af41ab042efdd8dc0
Carl Higgs <carl.higgs@rmit.edu.au>
2019-01-10 10:09:40 +1100

    updated script 21 for ncpf summary; now outputs to ncpf_mb2 and ncpf_region2 (the latter is final results we want to use)

--- 21_NCPF_BITRE_summary.py
+++ 21_NCPF_BITRE_summary.py
@@ -30,8 +30,8 @@ conn = psycopg2.connect(database=db, user=db_user, password=db_pwd)
 curs = conn.cursor()
 
 create_ncpf_mb_indicators = '''
-DROP TABLE IF EXISTS ncpf_mb;
-CREATE TABLE ncpf_mb AS
+DROP TABLE IF EXISTS ncpf_mb2;
+CREATE TABLE ncpf_mb2 AS
 SELECT a.mb_code_2016, 
 	   a.dwelling,
 	   a.person,
@@ -39,14 +39,13 @@ SELECT a.mb_code_2016,
        -- Also, nulls signify no access, hence these are coalesced to be included as zero
 	   a.dwelling * AVG(COALESCE((pt_any.distance < 400)::int,0))  AS dw_w_pt_any,
 	   a.dwelling * AVG(COALESCE((pt_freq.distance < 400)::int,0)) AS dw_w_pt_freq,
-	   a.dwelling * AVG(COALESCE(pos_any.ind_hard,0))              AS dw_w_pos_any,
-	   a.dwelling * AVG(COALESCE(pos_large.ind_hard,0))            AS dw_w_pos_large
+	   a.dwelling * AVG(pos_400m_ncpf.any) AS dw_w_pos_any,
+	   a.dwelling * AVG(pos_400m_ncpf.large) AS dw_w_pos_large
 FROM parcel_dwellings p
 LEFT JOIN abs_linkage a ON p.mb_code_20 = a.mb_code_2016
 LEFT JOIN (SELECT * FROM od_closest     WHERE dest_class = 'gtfs_2018_stops') pt_any   ON p.gnaf_pid = pt_any.gnaf_pid 
 LEFT JOIN (SELECT * FROM od_closest     WHERE dest_class = 'gtfs_2018_stop_30_mins_final') pt_freq  ON p.gnaf_pid = pt_freq.gnaf_pid 
-LEFT JOIN (SELECT * FROM od_closest_pos WHERE dest_class = 'any') pos_any ON p.gnaf_pid = pos_any.gnaf_pid 
-LEFT JOIN (SELECT * FROM od_closest_pos WHERE dest_class = 'large') pos_large ON p.gnaf_pid = pos_large.gnaf_pid 
+LEFT JOIN pos_400m_ncpf ON p.gnaf_pid = pos_400m_ncpf.gnaf_pid 
 WHERE NOT EXISTS (SELECT 1 
                     FROM excluded_parcels x 
                    WHERE x.gnaf_pid = p.gnaf_pid)
@@ -60,15 +59,15 @@ conn.commit()
 print("Done.")
 
 create_ncpf_region_summary = '''
-DROP TABLE IF EXISTS ncpf_region;
-CREATE TABLE ncpf_region AS
+DROP TABLE IF EXISTS ncpf_region2;
+CREATE TABLE ncpf_region2 AS
 SELECT SUM(dwelling)::int AS dwellings,
 	   SUM(person)::int   AS persons,
 	   ROUND((100*SUM(dw_w_pt_any   )/SUM(dwelling))::numeric, 2) AS pct_pt_any   ,
 	   ROUND((100*SUM(dw_w_pt_freq  )/SUM(dwelling))::numeric, 2) AS pct_pt_freq  ,
 	   ROUND((100*SUM(dw_w_pos_any  )/SUM(dwelling))::numeric, 2) AS pct_pos_any  ,
 	   ROUND((100*SUM(dw_w_pos_large)/SUM(dwelling))::numeric, 2) AS pct_pos_large
-FROM ncpf_mb;
+FROM ncpf_mb2;
 '''
 print("Create NCPF region results... "),
 curs.execute(create_ncpf_region_summary)
