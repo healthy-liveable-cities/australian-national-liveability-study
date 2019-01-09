4e489f4fd7df3a960f44bdbb2f5530db1dbcd39c
Carl Higgs <carl.higgs@rmit.edu.au>
2019-01-10 08:25:02 +1100

    updated batch script

--- batch_ncpf_pos_v2.bat
+++ batch_ncpf_pos_v2.bat
@@ -3,7 +3,6 @@ echo  Recompiled destinations and summarise for study regions
 echo  **NOTE: Assumes you have a current version of destinations database**
 
 FOR %%A  IN (%*) DO (
-  python 
 python ncpf_od_closest_pos_v2.py %%A any
 python ncpf_od_closest_pos_v2.py %%A large
 )
