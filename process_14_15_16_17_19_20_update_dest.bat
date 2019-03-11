91a5e7cf424df2d267f2970894cf3109bf2291e5
Carl Higgs <carl.higgs@rmit.edu.au>
2019-03-12 09:22:31 +1100

    updated script names in the 'update_dest' batch script, and added in script 20 (parcel exclusion) to the list, updating the name to reflect this addition

--- process_14_15_16_17_19_20_update_dest.bat
+++ process_14_15_16_17_19_20_update_dest.bat
@@ -3,17 +3,11 @@ echo  Process destination setup scripts and update od results and co-location ta
 
 FOR %%A  IN (%*) DO (
   python 14_recompile_destinations.py %%A
-  python 15_od_distances_closest_in_study_region.py %%A
-  python 16_od_count_in_buffer_distance.py %%A
+  python 15_od_distances_3200m.py %%A
+  python 16_od_distances_closest_in_study_region.py %%A
   python 17_aos_co-locations.py %%A
   python 19_neighbourhood_indicators.py %%A
+  python 20_parcel_exclusion.py %%A
 )
 
 @pause
-
-
-
-
-
-
-
