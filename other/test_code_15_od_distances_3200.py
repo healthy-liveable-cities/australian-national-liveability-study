44502270ca661d120e2107b53e678034328da649
Carl Higgs <carl.higgs@rmit.edu.au>
2019-03-14 18:09:33 +1100

    test code using pandas to aggregate od counts working

--- other/test_code_15_od_distances_3200.py
+++ other/test_code_15_od_distances_3200.py
@@ -6,11 +6,12 @@ db = 'li_albury_wodonga_2018'
 gdb_path = 'D:/ntnl_li_2018_template/data/study_region/albury_wodonga/li_albury_wodonga_2018.gdb'
 hex_grid = 'sua_2018_hex_3000m_diag' 
 # define a test destination
-dest_class = ('supermarket',)
+dest_class = ('supermarket',0)
 
 # set specific hex details
 hex = 265
 
+  
 import arcpy, arcinfo
 import os
 import time
@@ -18,6 +19,26 @@ import multiprocessing
 import sys
 import psycopg2 
 import numpy as np
+
+# make sure Network Analyst licence is 'checked out'
+arcpy.CheckOutExtension('Network')
+
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://{user}:{pwd}@{host}/{db}".format(user = db_user,
+                                                                      pwd  = db_pwd,
+                                                                      host = db_host,
+                                                                      db   = db))
+
+# function to group and aggregate integers to array
+# see https://stackoverflow.com/questions/22219004/grouping-rows-in-list-in-pandas-groupby
+def f(df,a,b):
+         df = df[[a,b]]
+         keys, values = df.sort_values(a).values.T
+         ukeys, index = np.unique(keys,True)
+         arrays = np.split(values,index[1:])
+         df2 = pandas.DataFrame({a:ukeys,b:[sorted(list(u)) for u in arrays]})
+         return df2
 
 # ArcGIS environment settings
 arcpy.env.workspace = gdb_path  
@@ -103,46 +124,18 @@ dest_count = dest_class[1]
 origin_dest_point_count = origin_point_count - dest_count
 dest_class = dest_class[0]
 destStartTime = time.time()
-if dest_count > 0:
-  curs.execute('''SELECT {id} 
-                  FROM parcel_dwellings p 
-                  WHERE hex_id = {hex}
-                  AND NOT EXISTS (SELECT 1 FROM {table} o 
-                                  WHERE hex = {hex}
-                                    AND dest_class = '{dest_class}' 
-                                    AND p.{id} = o.{id});
-               '''.format(table = result_table,
-                          id = origin_pointsID.lower(), 
-                          hex = hex,
-                          dest_class = dest_class))
-  remaining_parcels = [x[0] for x in list(curs)]
-  origin_subset = arcpy.SelectLayerByAttribute_management("origin_points_layer", 
-                                                          where_clause = "HEX_ID = {hex} AND {id} IN ('{parcels}')".format(hex = hex,
-                                                                                                                           id = origin_pointsID.lower(),
-                                                                                                                           parcels = "','".join(remaining_parcels)))
-  # OD Matrix Setup      
-  arcpy.AddLocations_na(in_network_analysis_layer = outNALayer, 
-      sub_layer                      = originsLayerName, 
-      in_table                       = origin_subset, 
-      field_mappings                 = "Name {} #".format(origin_pointsID), 
-      search_tolerance               = "{} Meters".format(tolerance), 
-      search_criteria                = "{} SHAPE;{} NONE".format(network_edges,network_junctions), 
-      append                         = "CLEAR", 
-      snap_to_position_along_network = "NO_SNAP", 
-      exclude_restricted_elements    = "INCLUDE",
-      search_query                   = "{} #;{} #".format(network_edges,network_junctions))
-else:
-  # OD Matrix Setup      
-  arcpy.AddLocations_na(in_network_analysis_layer = outNALayer, 
-      sub_layer                      = originsLayerName, 
-      in_table                       = origin_selection, 
-      field_mappings                 = "Name {} #".format(origin_pointsID), 
-      search_tolerance               = "{} Meters".format(tolerance), 
-      search_criteria                = "{} SHAPE;{} NONE".format(network_edges,network_junctions), 
-      append                         = "CLEAR", 
-      snap_to_position_along_network = "NO_SNAP", 
-      exclude_restricted_elements    = "INCLUDE",
-      search_query                   = "{} #;{} #".format(network_edges,network_junctions))
+
+# OD Matrix Setup      
+arcpy.AddLocations_na(in_network_analysis_layer = outNALayer, 
+    sub_layer                      = originsLayerName, 
+    in_table                       = origin_selection, 
+    field_mappings                 = "Name {} #".format(origin_pointsID), 
+    search_tolerance               = "{} Meters".format(tolerance), 
+    search_criteria                = "{} SHAPE;{} NONE".format(network_edges,network_junctions), 
+    append                         = "CLEAR", 
+    snap_to_position_along_network = "NO_SNAP", 
+    exclude_restricted_elements    = "INCLUDE",
+    search_query                   = "{} #;{} #".format(network_edges,network_junctions))
 
 # select destination points 
 destination_selection = arcpy.SelectLayerByAttribute_management(dest_in_hex, where_clause = "dest_class = '{}'".format(dest_class))
@@ -161,29 +154,26 @@ arcpy.AddLocations_na(in_network_analysis_layer = outNALayer,
 result = arcpy.Solve_na(outNALayer, terminate_on_solve_error = "CONTINUE")
 place = 'results were returned, now processing...'
 # Extract lines layer, export to SQL database
-outputLines = arcpy.da.SearchCursor(ODLinesSubLayer, fields)        
-chunkedLines = list()
-# Result table queries
-place = 'before outputLine loop'
-for outputLine in outputLines:
-  origin_id      = outputLine[0].split('-')[0].strip(' ')
-  dest_id   = outputLine[0].split('-')[1].split(',')
-  dest_class = dest_id[0].strip(' ')
-  distance  = int(round(outputLine[1]))
-  place = "before chunk append of returned and processed results"
-  chunkedLines.append('''('{origin_id}','{dest_class}',{distance})'''.format(origin_id = origin_id,
-                                                                            dest_class = dest_class,
-                                                                            distance  = distance))
-place = "before execute returned results sql"
-sql_query = '''{insert1}{hex}{insert2}{values}{insert3}{insert4}'''.format(insert1 = insert1,
-                                                                            hex = hex,
-                                                                            insert2 = insert2,
-                                                                            values   = ','.join(chunkedLines),
-                                                                            insert3 = insert3,
-                                                                            insert4 = insert4)
-curs.execute(sql_query)
-place = "before commit of returned and processed results"
-conn.commit()
+outputLines = arcpy.da.SearchCursor(ODLinesSubLayer, fields)      
+data = [x for x in outputLines]
+df = pandas.DataFrame(data = data, columns = ['od','distances'])
+df[[origin_pointsID,'d']] = df['od'].str.split(' - ',expand=True)
+# df[['d_name','d_num']] = df['d'].str.split(',',expand=True)
+# df = df[[origin_pointsID,'distances']]
+## Pandas groupby approach; by reputation slower than numpy
+# df = df.groupby('o')
+# df = df.aggregate(lambda x: tuple(x))
+
+# custom group function
+df = f(df,origin_pointsID,'distances')
+
+df["dest_class"] = dest_class
+df["hex"] = hex
+df = df[[origin_pointsID,'hex','dest_class','distances']]
+
+# append to pre-existing table
+df.to_sql("test_od",con = engine,index = False, if_exists='append')
+  
 # Where results don't exist for a destination class, ensure a null array is recorded
 null_dest_insert = '''
  INSERT INTO {table} ({id}, hex, dest_class, distances)  
