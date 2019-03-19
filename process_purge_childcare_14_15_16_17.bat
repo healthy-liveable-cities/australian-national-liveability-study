9eb2c9da2f1b62887e219e8921c7a304f5ce3860
Carl Higgs <carl.higgs@rmit.edu.au>
2019-03-19 15:27:06 +1100

    updates in case reqd

--- process_purge_childcare_14_15_16_17.bat
+++ process_purge_childcare_14_15_16_17.bat
@@ -1,8 +1,7 @@
-title Purge childcare destinations and process destination setup scripts and update od results and co-location table for study regions
-echo  Purge childcare destinations and process destination setup scripts and update od results and co-location table for study regions
+title Process destination setup scripts and update od results and co-location table for study regions
+echo  Process destination setup scripts and update od results and co-location table for study regions
 
 FOR %%A  IN (%*) DO (
-  python purge_childcare_destinations.py %%A
   python 14_recompile_destinations.py %%A
   python 15_od_distances_3200m.py %%A
   python 16_od_distances_closest_in_study_region.py %%A
