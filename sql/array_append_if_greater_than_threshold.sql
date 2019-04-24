9198ee08273ea15501cdab9109259c50bf269508
Carl Higgs <carl.higgs@rmit.edu.au>
2019-04-24 15:54:18 +1000

    added in applied sql example code for use of the array_append_if_gr sql function

--- sql/array_append_if_greater_than_threshold.sql
+++ sql/array_append_if_greater_than_threshold.sql
@@ -2,6 +2,14 @@ CREATE OR REPLACE FUNCTION array_append_if_gr(distances int[],distance int,thres
 BEGIN
   -- function to append an integer to an array of integers if it is larger than some given threshold 
   -- (ie. add in distance to closest to 3200m distances array if the distance to closest value is > 3200m
+  -- Example applied usage:
+  -- SELECT gnaf_pid, 
+         -- array_append_if_gr(dests.alcohol_offlicence,cl.alcohol_offlicence) AS array,
+         -- cl.alcohol_offlicence AS distance
+  -- FROM dest_distances_3200m dests 
+  -- LEFT JOIN dest_distance_m cl
+  -- USING (gnaf_pid) 
+  -- WHERE cl.alcohol_offlicence > 3200;
   IF ((distance <= threshold) OR (distance IS NULL)) 
       THEN RETURN distances;
   ELSE 
