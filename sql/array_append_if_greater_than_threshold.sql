6c4f37621ab21980beb4dc1e34f2d7ac53efea9d
Carl Higgs <carl.higgs@rmit.edu.au>
2019-04-24 15:46:07 +1000

    corrected description for the array_append_if_gr sql function

--- sql/array_append_if_greater_than_threshold.sql
+++ sql/array_append_if_greater_than_threshold.sql
@@ -1,15 +1,7 @@
--- return minimum value of an integer array (specifically here, used for distance to closest within 3200m)
 CREATE OR REPLACE FUNCTION array_append_if_gr(distances int[],distance int,threshold int default 3200) returns int[] as $$
 BEGIN
-  -- We check to see if the value we are exponentiation is more or less than 100; if so,
-  -- if so the result will be more or less either 1 or 0, respectively. 
-  -- If the value we are exponentiating is much > abs(700) then we risk overflow/underflow error
-  -- due to the value exceeding the numerical limits of postgresql
-  -- If the value we are exponentiating is based on a positive distance, then we know it is invalid!
-  -- For reference, a 10km distance with 400m threshold yields a check value of -120, 
-  -- the exponent of which is 1.30418087839363e+052 and 1 - 1/(1+exp(-120)) is basically 1 - 1 = 0
-  -- Using a check value of -100, the point at which zero is returned with a threshold of 400 
-  -- is for distance of 3339km
+  -- function to append an integer to an array of integers if it is larger than some given threshold 
+  -- (ie. add in distance to closest to 3200m distances array if the distance to closest value is > 3200m
   IF ((distance <= threshold) OR (distance IS NULL)) 
       THEN RETURN distances;
   ELSE 
