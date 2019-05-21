352b1fa7111b46840f2f609f97a54096ad71806d
Carl Higgs <carl.higgs@rmit.edu.au>
2019-05-21 13:32:01 +1000

    updated _diagnostics.py script

--- _diagnostics.py
+++ _diagnostics.py
@@ -10,7 +10,7 @@ import time
 import pandas
 from sqlalchemy import create_engine
 
-date_time = time.strftime("%Y%m%d-%H%M%S")
+date_time = time.strftime("%Y%m%d-%H%M")
 
 # Load settings from ind_study_region_matrix.xlsx
 xls = pandas.ExcelFile(os.path.join(sys.path[0],'ind_study_region_matrix.xlsx'))
@@ -28,42 +28,60 @@ db_pwd    = df_parameters.loc['db_pwd']['value'].encode('utf')
 
 who = sys.argv[1]
 locales = responsible[responsible == who].sort_values().index.values.tolist()
-outfile = '../data/study_region/indicator_summary_{}_{}_{}.xlsx'.format(who,'[subset]',date_time)
+outfile = '../data/study_region/indicator_summary_{}_{}.xlsx'.format(who,date_time)
 print('''
 Exporting to Excel file in location:
     {}
-'''.format(outfile))
-
-for locale in locales:
-    full_locale = df_studyregion.loc[locale]['full_locale'].encode('utf')
-    print('\n      - {}'.format(full_locale)),
-    db = 'li_{}_{}'.format(locale,year)
-    engine = create_engine("postgresql://{user}:{pwd}@{host}/{db}".format(user = db_user,
-                                                                 pwd  = db_pwd,
-                                                                 host = db_host,
-                                                                 db   = db))
-    for subset in ['','urban','not_urban']:
-        prefix = subset
-        if subset == '':
-            prefix = 'overall'
-        else:
-            subset = '_{}'.format(subset)
-        df = pandas.read_sql_query('''SELECT '{}' AS locale,
-                                            '{}' AS year,
-                                            '{}' AS database,
-                                            '{}' AS analyst,
-                                            unit_level_description, 
-                                            mean, 
-                                            sd, 
-                                            min, 
-                                            max, 
-                                            count, 
-                                            null_pct, 
-                                            count_pct 
-                                    FROM ind_summary{};
-                                '''.format(full_locale, year, db, who,subset), 
-                                    con=engine)
-        outfile = '../data/study_region/indicator_summary_{}_{}_{}.xlsx'.format(who,prefix,date_time)
-        df.to_excel(outfile,sheet_name='{}_{}'.format(locale,year))   
-        print("."),
+'''.format(outfile)),
+with pandas.ExcelWriter(outfile) as writer:
+    for locale in locales:
+        full_locale = df_studyregion.loc[locale]['full_locale'].encode('utf')
+        print('\n      - {}'.format(full_locale)),
+        db = 'li_{}_{}'.format(locale,year)
+        engine = create_engine("postgresql://{user}:{pwd}@{host}/{db}".format(user = db_user,
+                                                                     pwd  = db_pwd,
+                                                                     host = db_host,
+                                                                     db   = db))
+        df = pandas.read_sql_query('''SELECT '' AS locale,
+                                             '' AS year,
+                                             '' AS subset,
+                                             '' AS database,
+                                             '' AS analyst,
+                                              unit_level_description, 
+                                              mean, 
+                                              sd, 
+                                              min, 
+                                              max, 
+                                              count, 
+                                              null_pct, 
+                                              count_pct 
+                                        FROM ind_summary
+                                        LIMIT 0;
+                                    ''', 
+                                        con=engine)                                                         
+        for subset in ['','urban','not_urban']:
+            prefix = subset
+            if subset == '':
+                prefix = 'overall'
+            else:
+                subset = '_{}'.format(subset)
+            df = df.append(pandas.read_sql_query('''SELECT '{}' AS locale,
+                                                '{}' AS year,
+                                                '{}' AS subset,
+                                                '{}' AS database,
+                                                '{}' AS analyst,
+                                                unit_level_description, 
+                                                mean, 
+                                                sd, 
+                                                min, 
+                                                max, 
+                                                count, 
+                                                null_pct, 
+                                                count_pct 
+                                        FROM ind_summary{};
+                                    '''.format(full_locale, year, prefix, db, who,subset), 
+                                        con=engine))
+            print("."),
+        
+        df.to_excel(writer,sheet_name='{}_{}'.format(locale,year), index=False)   
 
