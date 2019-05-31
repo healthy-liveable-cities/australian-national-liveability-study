8baeb47395a922ac52aaf9c5f8c3f760422cd1b4
Carl Higgs <carl.higgs@rmit.edu.au>
2019-05-31 14:56:04 +1000

    update batch script to not run od analysis (want to test first)

--- nhsd_redux.bat
+++ nhsd_redux.bat
@@ -5,7 +5,6 @@ echo  **NOTE: Assumes you have the current version of destinations database, fro
 FOR %%A  IN (%*) DO (
   python purge_old_nhsd_20190531.py %%A
   python 14_recompile_destinations.py %%A
-  python 15_od_distances_3200m.py %%A
 )
 
 @pause
