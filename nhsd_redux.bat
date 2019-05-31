f072f22278e4478a0da75642562928563ab7959c
Carl Higgs <carl.higgs@rmit.edu.au>
2019-05-31 14:53:46 +1000

    added in batch script for purge, recompile and redo distance array for NHSD

---
+++ nhsd_redux.bat
@@ -0,0 +1,14 @@
+title NHSD redux: purge results, recompile, reanalyse distances for NHSD destinations
+echo  NHSD redux: purge results, recompile, reanalyse distances for NHSD destinations
+echo  **NOTE: Assumes you have the current version of destinations database, from 31 May 2019.**
+
+FOR %%A  IN (%*) DO (
+  python purge_old_nhsd_20190531.py %%A
+  python 14_recompile_destinations.py %%A
+  python 15_od_distances_3200m.py %%A
+)
+
+@pause
+
+
+
