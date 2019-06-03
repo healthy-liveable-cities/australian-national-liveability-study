97c2dde6e6707053e65b801896636c124ea821c9
Carl Higgs <carl.higgs@rmit.edu.au>
2019-06-03 11:05:08 +1000

    updated purge script to account for additional obsolete aged care and (for non-mildura locales) u3a destinations

--- purge_old_nhsd_20190531.py
+++ purge_old_nhsd_20190531.py
@@ -20,8 +20,10 @@ curs = conn.cursor()
 
 df_nhsd = pandas.read_excel(xls, 'nhsd classification',index_col=0)
 
-old_names = "'{}'".format("','".join(df_nhsd['old name'].values))
-
+old_names = "'{}','aged_care,'aged_care_residential_services'".format("','".join(df_nhsd['old name'].values))
+if locale!='mildura':
+    old_names = "{},'u3a','u3a_2019'".format(old_names)
+    
 sql = '''
 DELETE FROM od_closest WHERE dest_name IN ({old_names});
 DELETE FROM log_od_distances WHERE dest_name IN ({old_names});
