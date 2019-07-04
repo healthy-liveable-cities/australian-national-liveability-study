4de2fb4eeae14bdba89fddcf19fd402f79e46757
Carl Higgs <carl.higgs@rmit.edu.au>
2019-07-04 16:41:17 +1000

    updated disability and health script

--- process_dh_inds_20190704.bat
+++ process_dh_inds_20190704.bat
@@ -3,7 +3,6 @@ echo  Disability and health indicator output
 
 python grant_query.py %*
 FOR %%A  IN (%*) DO (
-  python 21_parcel_indicators.py %%A
   python 24_area_indicators.py %%A
   python disability_health_inds_sa1_dwellings.py %%A
 )
