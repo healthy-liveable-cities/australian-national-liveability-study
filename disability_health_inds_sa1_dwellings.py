897fa74e280d2847f3c2292c9b0373f1b736c1fa
Carl Higgs <carl.higgs@rmit.edu.au>
2019-07-05 16:25:04 +1000

    updated d&h output script

--- disability_health_inds_sa1_dwellings.py
+++ disability_health_inds_sa1_dwellings.py
@@ -118,13 +118,6 @@ FROM li_inds_sa1_dwelling;
 df = pandas.read_sql_query(sql,con=engine)
 df.to_csv('D:/ntnl_li_2018_template/data/dh_inds_{}_sa1_dwellings_2018_20190704.csv'.format(locale),index=False)
 
-sql = '''
-ALTER TABLE od_aos_jsonb ADD COLUMN IF NOT EXISTS locale text;
-UPDATE od_aos_jsonb SET locale = '{locale}';
-'''.format(locale = locale)
-curs.execute(sql)
-conn.commit()
-
 out_dir = 'D:/ntnl_li_2018_template/data/'
 out_file = 'ntnl_li_inds_{}_{}_Fc.sql'.format(locale,year)
 print("Creating sql dump to: {}".format(os.path.join(out_dir,out_file))),
