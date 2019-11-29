11525b3ac8aaee9172f4b51428bd648372d77038
Carl Higgs <carl.higgs@rmit.edu.au>
2019-11-29 14:26:53 +1100

    fixed batch script!

--- process_dest_update_20191129.bat
+++ process_dest_update_20191129.bat
@@ -7,7 +7,7 @@ FOR %%A  IN (%*) DO (
   python _ad_hoc_15_od_distances_3200m.py %%A
 )
 FOR %%A  IN (%*) DO (
-  python _ad_hoc_15_od_distances_3200m.py %%A
+  python _ad_hoc_16_od_distances_closest_in_study_region.py %%A
 )
 
 @pause
