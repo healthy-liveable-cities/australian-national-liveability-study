cfd66a046abd26a71ef87b227ff4f18fc3af1d66
Carl Higgs <carl.higgs@rmit.edu.au>
2020-01-10 19:23:06 +1100

    updated batch script to fix error (wrote python twice)

--- process_renew_2020-01-10.bat
+++ process_renew_2020-01-10.bat
@@ -2,12 +2,12 @@ title January 2020 Destination update - indicator calculation
 echo  January 2020 Destination update - indicator calculation
 
 FOR %%A  IN (%*) DO (
-  python python 14_recompile_destinations.py %%A
-  python python 15_od_distances_3200m_cl.py %%A
+  python 14_recompile_destinations.py %%A
+  python 15_od_distances_3200m_cl.py %%A
 )
 
 FOR %%A  IN (%*) DO (
-  python python 15_od_distances_3200m_cl.py %%A
+  python 15_od_distances_3200m_cl.py %%A
 )
 
 @pause
