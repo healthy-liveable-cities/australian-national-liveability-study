de2e2a800687924c0ef8260b257eea8091755c22
Carl Higgs <carl.higgs@rmit.edu.au>
2020-01-17 19:10:31 +1100

    updated abs indicator script

--- 19_ABS_indicators.py
+++ 19_ABS_indicators.py
@@ -29,22 +29,45 @@ engine = create_engine("postgresql://{user}:{pwd}@{host}/{db}".format(user = db_
                                                                  host = db_host,
                                                                  db   = db))
 
+area_codes  = pandas.read_sql('SELECT DISTINCT(sa1_7digitcode_2016)::int FROM area_linkage',engine)
+
+
 # import affordable housing indicator
 print("Affordable housing indicator... "),
-affordable_housing = pandas.read_csv('D:/ABS/data/2016/abs_liveability/housing_3040_sa1_all_20190712.csv', index_col=0)
+affordable_housing = pandas.read_csv('../data/ABS/derived/abs_2016_sa1_housing_3040_20190712.csv', index_col=0)
+affordable_housing = affordable_housing.loc[area_codes['sa1_7digitcode_2016']]
 affordable_housing.to_sql('abs_ind_30_40', con=engine, if_exists='replace')
 print("Done.")
 
-# Specify file names - assumed to be csv files
+# import mode of transport to work data
+print("Mode of transport to work indicators... "),
+mtwp = pandas.read_csv('../data/ABS/derived/abs_2016_sa1_mtwp_cleaned.csv', index_col=0)
+mtwp = mtwp.loc[area_codes['sa1_7digitcode_2016']]
+modes = ['active_transport','public_transport','vehicle','other_mode']
+mtwp['total_employed_travelling'] = mtwp[modes].sum(axis=1)
+for mode in modes[:-1]:
+  # percentage of employed persons travelling to work using this mode
+  mtwp['pct_{}'.format(mode)] = 100*mtwp[mode]/mtwp['total_employed_travelling']
 
+mtwp.to_sql('abs_mode_of_transport_to_work', con=engine, if_exists='replace')
+print("Done.")
+
+# import proportion renting indicator
+print("Proportion renting indicator... "),
+pct_renting = pandas.read_csv('../data/ABS/derived/abs_au_2016_tenure_type_by_sa1.csv', index_col=0)
+pct_renting = pct_renting.loc[area_codes['sa1_7digitcode_2016']]
+pct_renting['valid_total'] = pct_renting['Total'] - pct_renting['Not stated'] - pct_renting['Not applicable']
+pct_renting['pct_renting'] = 100*pct_renting['Rented']/pct_renting['valid_total']
+pct_renting.to_sql('abs_pct_renting', con=engine, if_exists='replace')
+print("Done.")
+
+
+print("Live and work in same local area indicator... "),
 indexName = 'sa1_7digitcode_2016'
-
-df = pandas.read_csv('D:/ABS/data/2016/abs_liveability/SA1 (UR) by SA3 (POW) - cleaned_20190712.csv', index_col=0)
+df = pandas.read_csv('../data/ABS/derived/SA1 (UR) by SA3 (POW) - cleaned_20190712.csv', index_col=0)
 df = df.reset_index()
 df = pandas.melt(df, id_vars=['sa1_7digitcode_2016'], value_vars=[x for x in df.columns if x!='sa1_7digitcode_2016'],var_name='sa3_work',value_name='count')
 df = df.astype(np.int64)
-
-print("Live and work in same local area indicator... "),
 
 # Get SA1 to SA3 look up table
 sql = '''
@@ -69,6 +92,10 @@ live_work= live_work.groupby(['sa1_7digitcode_2016','local'])['count'].agg(['sum
 live_work.columns = live_work.columns.droplevel()
 live_work = live_work.reset_index()
 live_work = live_work.set_index('sa1_7digitcode_2016')
+
+# filter down to area codes in region of interest
+local_live_work = live_work.loc[area_codes['sa1_7digitcode_2016']]
+
 live_work = live_work.fillna(0)
 live_work = live_work.astype(np.int64)
 live_work['total'] = live_work.apply(lambda x: x[False]+x[True],axis=1)
@@ -76,6 +103,18 @@ live_work['pct_live_work_local_area'] = live_work.apply(lambda x: 100*(x[True]/f
 
 live_work.to_sql('live_sa1_work_sa3', con=engine, if_exists='replace')
 print("Done.")
+
+
+print("Compile ABS indicators at SA1 level... "),
+dfs = [affordable_housing[['pct_30_40_housing']],
+       mtwp[['pct_active_transport','pct_public_transport','pct_vehicle']],
+       pct_renting[['pct_renting']],
+       live_work[['pct_live_work_local_area']]
+       ]
+abs_indicators = dfs[0].join(dfs[1:])
+
+print("Done.")
+
 # output to completion log
 script_running_log(script, task, start)
 conn.close()
